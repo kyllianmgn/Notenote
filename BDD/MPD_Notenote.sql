c0b7916dde1caee0c055a27455ccea037ad43d61
MAIGNAN_Ky <k.maignan@lprs.fr>
2022-03-08 17:01:49 +0100

    Ajout : Ajout de Composer et PHPMailer, MPD SQL, CSS

---
+++ BDD/MPD_Notenote.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE `kmn_notenote`;
+USE `kmn_notenote`;
+
+CREATE TABLE `etudiant`(
+  `id_etudiant` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `mdp` varchar(30) NOT NULL,
+  `ref_classe` int(11) NOT NULL,
+  PRIMARY KEY (`id_etudiant`)
+);
+
+CREATE TABLE `professeur`(
+   `id_professeur` int(11) NOT NULL AUTO_INCREMENT,
+   `nom` varchar(30) NOT NULL,
+   `prenom` varchar(30) NOT NULL,
+   `mail` varchar(30) NOT NULL,
+   `mdp` varchar(30) NOT NULL,
+   PRIMARY KEY (`id_professeur`)
+);
+
+CREATE TABLE `direction`(
+   `id_direction` int(11) NOT NULL AUTO_INCREMENT,
+   `nom` varchar(30) NOT NULL,
+   `prenom` varchar(30) NOT NULL,
+   `mail` varchar(30) NOT NULL,
+   `mdp` varchar(30) NOT NULL,
+    PRIMARY KEY (`id_direction`)
+);
+
+CREATE TABLE `cours`(
+   `id_cours` int(11) NOT NULL AUTO_INCREMENT,
+   `date` date NOT NULL,
+   `heure_debut` time NOT NULL,
+   `heure_fin` time NOT NULL,
+   `ref_classe` int(11) NOT NULL,
+   `ref_matiere` int(11) NOT NULL,
+   PRIMARY KEY (`id_cours`)
+);
+
+CREATE TABLE `dirige`(
+    `ref_professeur` int(11) NOT NULL,
+    `ref_cours` int(11) NOT NULL,
+    PRIMARY KEY (`ref_cours`,`ref_professeur`)
+);
+
+CREATE TABLE `matiere`(
+     `id_matiere` int(11) NOT NULL,
+     `nom` varchar(30) NOT NULL,
+     PRIMARY KEY (`id_matiere`)
+);
+
+CREATE TABLE `classe`(
+      `id_classe` int(11) NOT NULL,
+      `nom` varchar(30) NOT NULL,
+      PRIMARY KEY (`id_classe`)
+);
+
+CREATE TABLE `devoir`(
+    `id_devoir` int(11) NOT NULL AUTO_INCREMENT,
+    `description` varchar(30) NOT NULL,
+    `ref_professeur` int(11) NOT NULL,
+    `ref_classe` int(11) NOT NULL,
+    `ref_matiere` int(11) NOT NULL,
+    PRIMARY KEY (`id_devoir`)
+);
+
+ALTER TABLE `dirige`
+    ADD CONSTRAINT `fk_dirige_professeur` FOREIGN KEY (`ref_professeur`) REFERENCES `professeur` (`id_professeur`),
+    ADD CONSTRAINT `fk_dirige_cours` FOREIGN KEY (`ref_cours`) REFERENCES `cours` (`id_cours`);
+
+ALTER TABLE `cours`
+    ADD CONSTRAINT `fk_cours_matiere` FOREIGN KEY (`ref_matiere`) REFERENCES `matiere` (`id_matiere`),
+    ADD CONSTRAINT `fk_cours_classe` FOREIGN KEY (`ref_classe`) REFERENCES `classe` (`id_classe`);
+
+ALTER TABLE `devoir`
+    ADD CONSTRAINT `fk_devoir_classe` FOREIGN KEY (`ref_classe`) REFERENCES `classe` (`id_classe`),
+    ADD CONSTRAINT `fk_devoir_professeur` FOREIGN KEY (`ref_professeur`) REFERENCES `professeur` (`id_professeur`),
+    ADD CONSTRAINT `fk_devoir_matiere` FOREIGN KEY (`ref_matiere`) REFERENCES `matiere` (`id_matiere`);
