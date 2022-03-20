76a2d680f6163d9f02da9b35b01dfd609d77bcbb
Arvik <ugo.bercegeay@gmail.com>
2022-03-20 16:27:27 +0100

    Modif : modification de la classe abstraite Compte afin de suporter la classe abstraite Php_Table

--- Site/src/modele/compte/Compte.php
+++ Site/src/modele/compte/Compte.php
@@ -1,28 +1,11 @@
 <?php
 
-abstract class Compte{
+abstract class Compte extends Php_Table {
     private $nom;
     private $prenom;
     private $mail;
     private $mdp;
 
-    public function __construct(array $donnees){
-        $this->hydrate($donnees);
-    }
-
-    public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){
-            $method = 'set'.ucfirst($key);
-            if (method_exists($this,$method)){
-                $this->$method($value);
-            }
-        }
-    }
-
-    abstract public function ajout(BDD $bdd);
-    abstract public function afficher(BDD $bdd);
-    abstract public function modifier(BDD $bdd);
-    abstract public function supprimer(BDD $bdd);
     abstract public function connexion(BDD $bdd);
 
     /* @return mixed */
