99d4a02ea5a0d939a893ae0fa4d216dee12d8cf6
Arvik <ugo.bercegeay@gmail.com>
2022-03-20 16:26:20 +0100

    Ajout : ajout de la classe abstraite Php_Table

---
+++ Site/src/modele/Php_Table.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class Php_Table{
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this,$method)){
+                $this->$method($value);
+            }
+        }
+    }
+
+    abstract public function ajout(BDD $bdd);
+    abstract public function afficher(BDD $bdd);
+    abstract public function modifier(BDD $bdd);
+    abstract public function supprimer(BDD $bdd);
+}
