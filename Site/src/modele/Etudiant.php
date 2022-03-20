6926c93a22c7a2d0cc4bfa6a54326fc6c893e5de
Arvik <ugo.bercegeay@gmail.com>
2022-03-20 11:24:50 +0100

    Modif : modification de la class Etudiant afin de supporter la classe abstraite Compte

--- Site/src/modele/Etudiant.php
+++ Site/src/modele/Etudiant.php
@@ -1,25 +1,8 @@
 <?php
 
-class etudiant{
+class Etudiant extends Compte {
     private $id_etudiant;
-    private $nom;
-    private $prenom;
-    private $mail;
-    private $mdp;
     private $ref_classe;
-
-    public function __construct(array $donnees){
-        $this->hydrate($donnees);
-    }
-
-    public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){
-            $method = 'set'.ucfirst($key);
-            if (method_exists($this,$method)){
-                $this->$method($value);
-            }
-        }
-    }
 
     public function ajout(BDD $bdd){
         $req = $bdd->getBdd()->prepare("INSERT INTO etudiant(nom, prenom, mail, mdp, ref_classe) VALUES (:nom,:prenom,:mail,:mdp,:ref_classe);");
@@ -72,81 +55,25 @@ class etudiant{
             $this->setMdp($res['mdp']);
             $this->setRef_classe($res['ref_classe']);
             return true;
-        } else{
+        } else {
             return false;
         }
     }
 
-    /**
-     * @return mixed
-     */
+    /* @return mixed */
     public function getId_etudiant(){
         return $this->id_etudiant;
     }
-    /**
-     * @param mixed $id_etudiant
-     */
-    public function setId_etudiant($id_etudiant): void{
+    /* @param mixed $id_etudiant */
+    public function setId_etudiant($id_etudiant){
         $this->id_etudiant = $id_etudiant;
     }
-    /**
-     * @return mixed
-     */
-    public function getNom(){
-        return $this->nom;
-    }
-    /**
-     * @param mixed $nom
-     */
-    public function setNom($nom): void{
-        $this->nom = $nom;
-    }
-    /**
-     * @return mixed
-     */
-    public function getPrenom(){
-        return $this->prenom;
-    }
-    /**
-     * @param mixed $prenom
-     */
-    public function setPrenom($prenom): void{
-        $this->prenom = $prenom;
-    }
-    /**
-     * @return mixed
-     */
-    public function getMail(){
-        return $this->mail;
-    }
-    /**
-     * @param mixed $mail
-     */
-    public function setMail($mail): void{
-        $this->mail = $mail;
-    }
-    /**
-     * @return mixed
-     */
-    public function getMdp(){
-        return $this->mdp;
-    }
-    /**
-     * @param mixed $mdp
-     */
-    public function setMdp($mdp): void{
-        $this->mdp = $mdp;
-    }
-    /**
-     * @return mixed
-     */
+    /* @return mixed */
     public function getRef_classe(){
         return $this->ref_classe;
     }
-    /**
-     * @param mixed $ref_classe
-     */
-    public function setRef_classe($ref_classe): void{
+    /* @param mixed $ref_classe */
+    public function setRef_classe($ref_classe){
         $this->ref_classe = $ref_classe;
     }
 }
